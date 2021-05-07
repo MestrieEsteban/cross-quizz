89256baaa88f1dcef46d5a857e64e25cabfb9c23
Alexis3490 <alexispiraux72@gmail.com>
2021-05-07 15:41:35 +0200

    update

--- Front/src/layouts/MainLayout.vue
+++ Front/src/layouts/MainLayout.vue
@@ -1,5 +1,10 @@
 <template>
   <q-layout view="lHh Lpr lFf">
+    <q-header>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@5.8.55/css/materialdesignicons.min.css">
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css">    </q-header>
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -7,6 +12,7 @@
 </template>
 
 <script>
+import 'buefy/dist/buefy.css'
 export default {
   name: 'MainLayout',
 }
--- Front/src/router/index.js
+++ Front/src/router/index.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Buefy from 'buefy';
 
 import routes from './routes'
 
 Vue.use(VueRouter)
+Vue.use(Buefy)
 
 /*
  * If not building with SSR mode, you can
