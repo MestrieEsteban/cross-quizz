4bd1215d04ba5a400c37146b9242dc39b4ce7f0c
Alexis3490 <alexispiraux72@gmail.com>
2021-05-09 12:10:46 +0200

    update

--- Front/src/router/routes.js
+++ Front/src/router/routes.js
@@ -1,6 +1,5 @@
 
 const routes = [
-<<<<<<< HEAD
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
@@ -31,20 +30,6 @@ const routes = [
       { path: ':name/:id', props: true, component: () => import('pages/quiz/quiz.vue')},
     ]
   },
-
-=======
-	{
-		path: '',
-		component: () => import('layouts/MainLayout.vue'),
-		children: [
-			{ path: '', component: () => import('src/pages') },
-			{ path: 'user/inscription', component: () => import('pages/user/inscription.vue') },
-			{ path: 'user/connexion', component: () => import('pages/user/login.vue') },
-			{ path: 'quiz/lesQuiz', component: () => import('pages/quiz/lesQuiz.vue') },
-			{ path: 'quiz/:id', name: 'quiz', props: true, component: () => import('pages/quiz/quiz.vue') },
-		]
-	},
->>>>>>> 36ef57d78d2e6c40ee6a92c9749ab9c7a9028ce4
 
 	// Always leave this as last one,
 	// but you can also remove it
